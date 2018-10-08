ae2e165318d0634212fbc02aba1dd298a3b069ff
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2018-10-09 01:46:45 +0530

    Add files via upload

---
+++ Variable_Example.java
@@ -0,0 +1,15 @@
+/* This is Variable_Example */
+public class Variable_Example {
+
+	public static void main(String[] args) 
+	{
+		int num;   //this declares a variable called num
+		num=100;
+		System.out.println("This is num:"+num);
+		num=num*2;
+	System.out.println("The Value of num*2 is:"+num);
+	System.out.println("num");
+
+	}
+
+}
