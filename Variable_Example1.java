5f0b6ed1eeeb6855e7a78179c6c9d57413d94593
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2018-10-09 01:48:56 +0530

    Update Variable_Example1.java

--- Variable_Example1.java
+++ Variable_Example1.java
@@ -5,7 +5,7 @@ public class Variable_Example
 	public static void main(String[] args) 
 	{
 		int num;   //this declares a variable called num
-		num=100;
+		num=100;   //this assigns num the value 100
 		System.out.println("This is num:"+num);
 		num=num*2;
 	System.out.println("The Value of num*2 is:"+num);
