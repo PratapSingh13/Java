10ac300e4232c133f5dceedf1751ebcace974f29
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2018-10-10 02:12:20 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,21 @@
+/* This Demonstrates a Variable */
+public class Variable 
+{
+
+	public static void main(String[] args) 
+	{
+		int var1;   //This Declares a Variable
+		int var2;  //This Declares another Variable
+		
+		var1=1024;  //This Assigns 1024 to var1
+		
+		System.out.println("var1 contains"+var1);
+		
+		var2=var1/2;
+		System.out.println("var2 contains var1/2:");
+		System.out.println(var2);
+		
+
+	}
+
+}
