1d53269cb03e7081ba13acfa267e14d866114529
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2018-11-08 12:46:32 +0530

    Add files via upload

---
+++ Short_Hand_OPERATORS.java
@@ -0,0 +1,24 @@
+// ShorHand Operators +=, -=, *=........
+public class Short_Hand_OPERATORS 
+{
+
+	public static void main(String[] args) 
+	{
+		int i=5;     
+		int j=10;
+		int k=20;
+		int l=100;
+		
+		i +=3;         // i = i+3
+		j -=4;        // j = j-4
+		k *=5;       // k = k*5
+		l /=10;     // l = l/10  
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+
+	}
+
+}
