8a810600e4cbb3b5fc7c3c89f26368fcc1df509b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2018-11-08 12:45:07 +0530

    Add files via upload

---
+++ Use_of_Printf.java
@@ -0,0 +1,15 @@
+
+public class Use_of_Printf 
+{
+
+	public static void main(String[] args) 
+	{
+		int i,j,k;
+		i=5;
+		j=4;
+		k=i+j;
+		System.out.println("The addition of "+i+ " and " +j+ " is "+k);
+		System.out.printf("The addition of %d and %d is %d",i,j,k);
+	}
+
+}
