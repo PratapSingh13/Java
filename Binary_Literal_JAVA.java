aeee9f42f5a6b20ddef45d3975527ef82d990465
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2018-11-08 12:50:37 +0530

    Add files via upload

---
+++ Binary_Literal_JAVA.java
@@ -0,0 +1,11 @@
+
+public class Binary_Literal_JAVA 
+{
+
+	public static void main(String[] args)
+	{
+		int i= 0B1000000;     // Necessary to use 0b or 0B before Binary Number 
+		System.out.println(i);
+	}
+
+}
