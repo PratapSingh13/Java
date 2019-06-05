9a4e3746d0efeced07a67ab2b389541ebb777b25
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-05 10:24:23 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+//TypeCasting=Conversion
+public class Typecasting 
+{
+	public static void main(String[] args) 
+	{
+		double d=200.45;
+		long l=(long)d;   //Narrowing TypeCasting-Double to Long 
+		int i=(int)d;     //Narrowing TypeCasting-Double to Int
+		System.out.println(d);
+		System.out.println(l);
+		System.out.println(i);
+	}
+
+}
