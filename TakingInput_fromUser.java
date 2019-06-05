28b9131a6046203e549abac54f7c483ed7a7ed7a
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-05 10:26:28 +0530

    Add files via upload

---
+++ TakingInput_fromUser.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;        //Importing Library of JAVA with Scanner class
+public class TakingInput_fromUser 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the FIRST Number");
+		int First=input.nextInt();
+		
+		System.out.println("Enter the FIRST Number");
+		int Second=input.nextInt();
+		
+		System.out.println(First+Second);
+
+	}
+
+}
