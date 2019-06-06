8e2bb82eeb9ba72016448e5b55493fd61313bf99
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-06 23:45:41 +0530

    Add files via upload

---
+++ Local_Variable.java
@@ -0,0 +1,18 @@
+
+public class Local_Variable 
+{
+	public void StudentAge()
+	{
+		int age=0;
+		age=age+5;
+		System.out.println("Student age is :"+age);
+	}
+	
+	public static void main(String[] args) 
+	{
+		Local_Variable obj=new Local_Variable();
+		obj.StudentAge();
+
+	}
+
+}
