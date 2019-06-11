6a1ab6c79d987c0378a5cd4429e9c3d495c2c9d7
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-11 17:18:51 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,26 @@
+//Example of Single Inheritance
+class Shape
+{
+	int length;
+	int breadth;
+}
+public class Rectangle extends Shape
+{
+    int area;
+    public void CalculateArea()
+    {
+    	area=length*breadth;
+    }
+	public static void main(String[] args) 
+	{
+		Rectangle r = new Rectangle();
+		//Assigning the values to Shape Class Attributes
+		r.length=20;
+		r.breadth=10;
+		// Calculate Area
+		r.CalculateArea();
+		System.out.println("The Area of Rectangle of Length\""+r.length+"\"and Breadth\""+r.breadth+"\"is\""+r.area+"\"");
+
+	}
+
+}
