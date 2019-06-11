65efdc4b8a9271cbbc29334a377e9a9a73d3a9f1
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-11 16:36:34 +0530

    Add files via upload

---
+++ Java_Teacher.java
@@ -0,0 +1,24 @@
+//Inheritance Example
+class Teacher
+{
+	String Designation="Teacher";
+	String CollegeName="KIET";
+	public void does()
+	{
+		System.out.println("Teaching");
+	}
+}
+public class Java_Teacher extends Teacher
+{
+    String mainsubject="JAVA";	
+	public static void main(String[] args) 
+	{
+		Java_Teacher obj=new Java_Teacher();
+		System.out.println(obj.CollegeName);
+		System.out.println(obj.Designation);
+		System.out.println(obj.mainsubject);
+		obj.does();
+
+	}
+
+}
