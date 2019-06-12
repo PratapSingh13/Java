29edac09be7ab91fc7c3e03c2b37efc29335197b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-12 23:25:53 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,23 @@
+
+public class Animal 
+{
+	public void sound()
+	{
+		System.out.println("Animals are making Sound");
+	}
+	class Horse extends Animal
+	{
+		public void Sound()
+		{
+			System.out.println("Neigh");
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		Animal obj = new Horse();
+		obj.sound();
+
+	}
+
+}
