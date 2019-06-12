421e5f1bc56a5f3a608a0000999df45fba8166c0
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-13 02:00:29 +0530

    Add files via upload

---
+++ Encapsulation_Demo.java
@@ -0,0 +1,45 @@
+class Main
+{
+	private int serialnum;
+	private int age;
+	private String name;
+	public int getEmpserialnum()
+	{
+		return serialnum;
+	}
+	public String getEmpName()
+	{
+		return name;
+	}
+	public int getEmpAge()
+	{
+		return age;
+	}
+	public void setEmpAge(int newValue)
+	{
+		age=newValue;
+	}
+	public void setEmpName(String newValue)
+	{
+		name=newValue;
+	}
+	public void setEmpSSN(int newValue)
+	{
+		serialnum=newValue;
+	}
+}
+public class Encapsulation_Demo {
+
+	public static void main(String[] args) 
+	{
+		Main obj=new Main();
+		obj.setEmpName("PRATAP");
+		obj.setEmpAge(21);
+		obj.setEmpSSN(1802914980);
+		System.out.println("Employee Name:"+obj.getEmpName());
+		System.out.println("Employee Serial Number:"+obj.getEmpserialnum());
+		System.out.println("Emploee Age:"+obj.getEmpAge());
+
+	}
+
+}
