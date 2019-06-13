cb611a4f0b951391371c13ec7923db70cd40c3d9
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-14 01:09:15 +0530

    Add files via upload

---
+++ Withot_ExceptionHandling.java
@@ -0,0 +1,16 @@
+//Problem without Exception Handling
+public class Withot_ExceptionHandling
+{
+	public static void main(String[] args) 
+	{
+		//Here Exception may occur
+		int data=50/0;        //Exception
+		System.out.println("Rest of the Code");
+	}
+}
+/* Rest of the Code is not executed
+ * (In such case, the Rest of the Code statement is not Printed)
+ */
+/*There can be 100 lines of code after exception. 
+ * so all the code after exception will not be executed
+ */
