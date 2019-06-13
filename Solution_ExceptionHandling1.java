ffe0ccdfc4244a8f550b902a89911eaea395b4a4
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-14 01:07:48 +0530

    Add files via upload

---
+++ Solution_ExceptionHandling1.java
@@ -0,0 +1,18 @@
+// Java Program to demonstrate TRY & CATCH
+public class Solution_ExceptionHandling1 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			// Exception may be here
+			int data=50/0;
+		}
+		//Handling the Exception
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		System.out.println("Rest of the Code");
+	}
+}
