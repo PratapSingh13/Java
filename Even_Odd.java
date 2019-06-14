fdd69b7e14920a863e8bb5be0f845524d5dadf5b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-14 23:30:51 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,17 @@
+//Simple Java Program to find Number is either Even or Odd
+public class Even_Odd 
+{
+	public static void main(String[] args) 
+	{
+		int x=21;
+		if(x%2==0)
+		{
+			System.out.println("Number is Even");
+		}
+		else
+		{
+			System.out.println("Number is odd");
+		}
+	}
+
+}
