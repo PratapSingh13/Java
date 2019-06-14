caafd5cefd250ceeeaeee0b7dff44230af71bb1a
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-15 00:09:22 +0530

    Add files via upload

---
+++ Biggest_Number.java
@@ -0,0 +1,33 @@
+//Java Program to find the biggest Number
+import java.util.Scanner;
+public class Biggest_Number 
+{
+	public static void main(String[] args) 
+	{
+		int x,y,z;
+		System.out.println("Enter any three Numbers");
+		Scanner in = new Scanner (System.in);
+		x=in.nextInt();
+		y=in.nextInt();
+		z=in.nextInt();
+		if(x>y && x>z)
+		{
+			System.out.println("First Number is Largest");
+		}
+		else
+			if(y>x && y>z)
+			{
+				System.out.println("Second Number is Largest");
+			}
+			else
+				if(z>x && z>y)
+				{
+					System.out.println("Third Number is Largest");
+				}
+				else
+				{
+					System.out.println("The Numbers are not distinct");
+				}
+	}
+
+}
