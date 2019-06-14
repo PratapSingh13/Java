9080f248ee4e2027b9f392d9ea769ef9494f3267
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-14 23:31:14 +0530

    Add files via upload

---
+++ Input_Even_Odd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Input_Even_Odd 
+{
+	public static void main(String[] args) 
+	{
+		int x;
+		System.out.println("Input a Number Please");
+		Scanner in = new Scanner(System.in);
+		x=in.nextInt();
+		if(x%2==0)
+		{
+			System.out.println("The Number is Even");
+		}
+		else
+		{
+			System.out.println("The Number is Odd");
+		}
+	}
+
+}
