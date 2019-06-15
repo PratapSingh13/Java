6ae11b83622e51bcc47f5f59c24ff2d0646aec47
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-16 00:07:24 +0530

    Add files via upload

---
+++ Java_GUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+public class Java_GUI 
+{
+	public static void main(String[] args) 
+	{
+		JFrame frame=new JFrame("My First GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		JButton button1= new JButton("Button 1");
+		JButton button2= new JButton("Button 2");
+		JButton button=new JButton("Press");
+		frame.getContentPane().add(button1);
+		frame.getContentPane().add(button2);
+		frame.setVisible(true);
+
+	}
+
+}
