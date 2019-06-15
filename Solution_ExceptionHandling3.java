10dcad71ce9dcc25f88bf7b5a8537a85c58607a6
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-16 00:09:50 +0530

    Add files via upload

---
+++ Solution_ExceptionHandling3.java
@@ -0,0 +1,19 @@
+
+public class Solution_ExceptionHandling3 
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			//Exception may throw here
+			int data=50/0;
+		}
+		//Handling the Exception using Exception Class
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		System.out.println("Rest of the Code");
+	}
+
+}
