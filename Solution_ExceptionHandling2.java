b69c9ec6eb96fee8f57982cfc16bd8b0ce5228de
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-16 00:09:15 +0530

    Add files via upload

---
+++ Solution_ExceptionHandling2.java
@@ -0,0 +1,19 @@
+
+public class Solution_ExceptionHandling2 
+{
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			//Exception may be Here
+			int data=50/0;
+			//if exception occurs,the remaining statement will not exceute  
+			System.out.println("Rest of the Code");
+		}
+		//Handling Exception
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
