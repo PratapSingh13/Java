def0dd056508306b015955ac3c6de58052b7debe
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-17 18:35:09 +0530

    Add files via upload

--- WhileLoopDemo.java
+++ WhileLoopDemo.java
@@ -1,18 +1,13 @@
 //Java Program to demonstrate While Loop
 public class WhileLoopDemo 
 {
-
 	public static void main(String[] args) 
 	{
-		int n=1;
+		int n=11;
 		while(n<=10)
 		{
 			System.out.println(n);
 			n=n+1;
 		}
-		
-			
-
 	}
-
 }
