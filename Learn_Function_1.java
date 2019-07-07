9ee07575aeb36bf4404bb940c8e8c1c0f4d0a590
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-07 19:56:28 +0530

    Add files via upload

---
+++ Learn_Function_1.java
@@ -0,0 +1,25 @@
+
+public class Learn_Function_1 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=11;
+		int b=6;
+		int c=mainFunction(a,b);
+		System.out.println("Minimum Value=" +c);
+
+	}
+	/* Return of Minimum of two numbers*/
+	public static int mainFunction(int n1, int n2)
+	{
+		int min;
+		if(n1>n2)
+			min=n2;
+		else
+			min=n1;
+		return min;
+	}
+	 
+
+}
