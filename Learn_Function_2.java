f1c1ce722a4ba14d75f76a766506ca8b4b28022d
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-07 19:56:52 +0530

    Add files via upload

---
+++ Learn_Function_2.java
@@ -0,0 +1,21 @@
+
+public class Learn_Function_2 
+{
+
+	public static void main(String[] args) 
+	{
+		RankPoints(255.7);       //Method of name RankPoints
+	}
+	public static void RankPoints(double points)
+	{
+		if (points >= 122.4)
+		{
+			System.out.println("Rank:A2");
+		}
+		else
+		{
+			System.out.println("Rank:A3");
+		}
+	}
+
+}
