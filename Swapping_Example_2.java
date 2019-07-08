58ef32f97a108717f08344aa239ab389bc0b8bbf
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-08 21:06:54 +0530

    Add files via upload

---
+++ Swapping_Example_2.java
@@ -0,0 +1,17 @@
+// JAVA Program to SWAP two Numbers
+// WAY-2
+public class Swapping_Example_2 
+{
+	public static void main(String[] args) 
+	{
+		int a=11;
+		int b=12;
+		System.out.println("Before SWAPPING: a = "+a + " and b = "+b);
+		a=a+b;  // a=23
+		b=a-b;  // b=11
+		a=a-b;  // a=1
+		System.out.println("After SWAPPING: a = "+a + " and b = "+b);
+
+	}
+
+}
