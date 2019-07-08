e438d0e4dffb6a1b30cb1860b2c150c5cd74b6fc
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-08 21:04:46 +0530

    Add files via upload

---
+++ Pattern_Printing_2.java
@@ -0,0 +1,24 @@
+
+public class Pattern_Printing_2 
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+		int k;
+		for(i=1; i<=5; i++)
+		{
+			for(j=5; j>i; j--)
+			{
+				System.out.print(" ");
+			}
+			for(k=1; k<=i; k++)
+			{
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
