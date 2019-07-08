30db038ae89e083a651573d62a1b40a92d60ce67
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-08 21:04:16 +0530

    Add files via upload

---
+++ Pattern_Printing_1.java
@@ -0,0 +1,17 @@
+// JAVA Program to Print the Pattern of *
+public class Pattern_Printing_1 
+{
+	public static void main(String args[])
+	{
+		int i;
+		int j;
+		for(i=1; i<=3; i++)
+		{
+			for(j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}	
