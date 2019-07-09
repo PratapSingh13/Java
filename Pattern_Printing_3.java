bc1099bd6429a6fa42ce9faa6187ed018d122e38
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-09 20:39:58 +0530

    Add files via upload

---
+++ Pattern_Printing_3.java
@@ -0,0 +1,25 @@
+
+public class Pattern_Printing_3 
+{
+
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+		int k;
+		for(i=1; i<=5; i++)
+		{
+			for(k=5; k>=i; k--)
+			{
+				System.out.print("*");
+			}
+			for(j=1; j<i; j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
