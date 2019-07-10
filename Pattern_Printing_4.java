005089cfaa8918bd6a93e01610acd72d7ed09af5
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-10 21:28:44 +0530

    Add files via upload

--- Pattern_Printing_4.java
+++ Pattern_Printing_4.java
@@ -6,15 +6,19 @@ public class Pattern_Printing_4
 		int k;
 		for(i=1; i<=10; i++)
 		{
+			//Print Star in Decreasing Order
 			for(j=10; j>i; j--)
 			{
 				System.out.print(" ");
 			}
+			//Print Star in Increasing Order
 			for(k=1; k<=(i*2)-1; k++)
 			{
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+
 	}
+
 }
