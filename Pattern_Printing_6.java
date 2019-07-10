f7bc5b8be3994288acc01d6b3ae18c2e81e65cb2
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-10 21:30:08 +0530

    Add files via upload

---
+++ Pattern_Printing_6.java
@@ -0,0 +1,26 @@
+
+public class Pattern_Printing_6 
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+		int k;
+		for(i=6; i>=1; i--)
+		{
+			//Print Star in Decreasing Order
+			for(k=1; k<=(i*2)-1; k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			//Print Space in increasing order
+			for(j=6; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+		}
+
+	}
+
+}
