f6efcd73f8256960452a28ac43fab797cf7fcd42
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-10 21:29:30 +0530

    Add files via upload

---
+++ Pattern_Printing_5.java
@@ -0,0 +1,26 @@
+
+public class Pattern_Printing_5 {
+
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+		int k;
+		for(i=1; i<=5; i++)
+		{
+			//Print Space in increasing Order
+			for(j=1; j<i; j++)
+			{
+				System.out.print(" ");
+			}
+			//Print Star in Decreasing Order
+			for(k=5; k>=i; k--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
