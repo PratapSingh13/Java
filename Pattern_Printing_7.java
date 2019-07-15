7c3c96db5aaf10c825a8b3af68c18252e91fd70b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-15 20:22:25 +0530

    Add files via upload

---
+++ Pattern_Printing_7.java
@@ -0,0 +1,29 @@
+
+public class Pattern_Printing_7 
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+		for(i=1; i<=6; i++)
+		{
+			for(j=1; j<=i; j++)
+			{
+				//Print Star in Increasing Order
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(i=1; i<=(6-1); i++)
+		{
+			//Print Star in Decreasing Order
+			for(j=(6-1); j>=i; j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
