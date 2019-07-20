f41c6cbdefaa5e976ddeb55ad0350fa33f67a0ab
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-20 20:48:41 +0530

    Add files via upload

---
+++ Array_Example_3.java
@@ -0,0 +1,17 @@
+//Java Program to print the array elements using for-each loop 
+public class Array_Example_3 
+{
+
+	public static void main(String[] args) 
+	{
+		int arr[]= {1,2,3,4,5};
+		
+		//Printing Array using for-each loop
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
