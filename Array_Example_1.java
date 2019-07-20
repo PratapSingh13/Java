cd622ae7f8592f2ef4147659be8414236292f1ba
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-20 20:47:20 +0530

    Add files via upload

---
+++ Array_Example_1.java
@@ -0,0 +1,22 @@
+
+public class Array_Example_1 
+{
+
+	public static void main(String[] args) 
+	{
+		//Declaration and Instantiation
+		int a[]=new int[5];
+		//Initialization
+		a[0]=10;
+		a[1]=20;
+		a[2]=70;
+		a[3]=40;
+		a[4]=50;
+		//Array Traversing
+		for(int i=0; i<a.length; i++)  //Length is the Property of the Array
+			System.out.println(a[i]);
+				
+
+	}
+
+}
