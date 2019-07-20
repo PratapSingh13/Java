c035c17c6e7cb58fa8f11b7d8d1c54b8bb343123
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-20 20:48:01 +0530

    Add files via upload

---
+++ Array_Example_2.java
@@ -0,0 +1,19 @@
+//Java Program to show Declaration,Instantiation, Initialization of Array
+public class Array_Example_2 
+{
+
+	public static void main(String[] args) 
+	{
+		//Declaration,Instantiation, Initialization
+		int a[]= {1,2,3,4,5};
+		
+		//Length is the Property of the array
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+			
+
+	}
+
+}
