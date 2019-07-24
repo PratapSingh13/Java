2295327eaa5678b207d949e80764f776fced818a
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-07-24 21:16:51 +0530

    Update Logical_OperatorsExample.java

--- Logical_OperatorsExample.java
+++ Logical_OperatorsExample.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 class gui
 {
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
        JFrame frame = new JFrame("My First GUI");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(300,300);
