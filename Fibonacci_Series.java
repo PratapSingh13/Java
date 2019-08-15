779395ca396a5e8a35458a80fcd2343828ae547f
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-16 03:05:13 +0530

    Add files via upload

---
+++ Fibonacci_Series.java
@@ -0,0 +1,25 @@
+//Java Program for Fibonacci series
+import java.util.Scanner;
+public class Fibonacci_Series 
+{
+	public static void main(String[] args) 
+	{
+		int num;
+		int a=0;
+		int b=0;
+		int c=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of Times");
+		num=sc.nextInt();
+		System.out.println("Fibonacci Series of the Number is:");
+		for(int i=0; i<=num; i++)
+		{
+			a=b;
+		    b=c;
+		    c=a+b;
+		    System.out.println(a +"");
+		}   
+
+	}
+
+}
