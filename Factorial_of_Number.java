5da0afc9d958a27dcae347df1aed8fff2717133a
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-15 18:54:01 +0530

    Add files via upload

---
+++ Factorial_of_Number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Factorial_of_Number 
+{
+	public static void main(String[] args) 
+	{
+		int n, c;
+		int fact = 1;
+		System.out.println("Enter an Integer to calculaate it's Factorial");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		if(n<0)
+			System.out.println("Number should be non-negative");
+		else
+		{
+			for(c=1; c<=n; c++)
+				fact=fact*c;
+			System.out.println("Factorial of "+n+" is = "+fact);
+		}
+
+	}
+
+}
