c74399fc8b4b5b8fed46ec9c2b27e584021be317
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-16 20:01:28 +0530

    Add files via upload

---
+++ Prime_Numbers_List.java
@@ -0,0 +1,26 @@
+
+public class Prime_Numbers_List 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int n=1; n<=100; n++)
+		{
+			int temp=0;
+			for(int i=2; i<=n-1; i++)
+			{
+				if(n%i==0)
+				{
+					temp=temp+1;
+				}
+			}
+			if(temp==0)
+			{
+				System.out.println(n);
+			}
+			
+		}
+		
+	}
+
+}
