eb7cca34a1a22c0f3a4821ab21bd51b5519ab622
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-16 19:39:26 +0530

    Add files via upload

---
+++ Prime_Number.java
@@ -0,0 +1,29 @@
+//Java Program to Check either Number is Prime or not
+import java.util.Scanner;
+public class Prime_Number 
+{
+	public static void main(String args[]) 
+	{
+		int n;
+		int temp=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number which you want to check either the number is Prime or Not !");
+		n=sc.nextInt();
+		for(int i=2; i<=(n-1); i++)
+		{
+			if(n%i==0) 
+			{
+				temp=temp+1;
+			}
+		}
+		if(temp==0)
+		{
+			System.out.println("Your Number is Prime");
+		}
+		else
+		{
+			System.out.println("Your Number is not Prime");
+		}
+	}
+
+}
