460bcea5d9d624b780d56572e57176621913dbf9
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-18 17:47:42 +0530

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,31 @@
+//Java Program to show MultiLevel Inheritance
+class Animal
+{
+	void eat() 
+	{
+		System.out.println("Animals are eating");
+	}
+}
+class Dog extends Animal
+{
+	void sound()
+	{
+		System.out.println("Dogs are Barking");
+	}
+}
+class Bird extends Dog
+{
+	void fly() 
+	{
+		System.out.println("Birds are Flying");
+	}
+
+	public static void main(String[] args) 
+	{
+		Bird obj=new Bird();
+		obj.eat();
+		obj.sound();
+		obj.fly();
+	}
+
+}
