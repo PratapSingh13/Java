2030db783c4833b1429c4f1145aa8f7ab3e70176
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-21 23:10:38 +0530

    Add files via upload

---
+++ String_Reverse.java
@@ -0,0 +1,16 @@
+//Java Program to show how to Reverse a String
+public class String_Reverse 
+{
+	public static void main(String[] args) 
+	{
+		String name="Yogendra Pratap Singh";
+		int size = name.length();           //Using length() we get the size of String
+		String rev="";                       
+		for(int i=size-1; i>=0; i--)
+		{
+			rev=rev+name.charAt(i);        //Using char() we get the character of String
+		}
+		System.out.println(rev);
+	}
+
+}
