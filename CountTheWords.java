40b8ea8c378208a3c83ae91c0fc1aa7b23c7fbbe
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-22 23:56:21 +0530

    Update CountTheWords.java

--- CountTheWords.java
+++ CountTheWords.java
@@ -4,10 +4,11 @@ public class CountTheWords
 {
 	public static void main(String[] args)
 	{
+		String str;
 		System.out.println("Enter the String");
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		String[]words=str.trim().split(" ");
+		String[]words=str.trim().split(" ");        //
 		System.out.println("Number of Words in the String is: " +words.length);
 				
 	}
