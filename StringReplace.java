6abda4c3753566e2fe65f9ec805a3dea616245e9
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-23 01:20:48 +0530

    Add files via upload

---
+++ StringReplace.java
@@ -0,0 +1,12 @@
+//Java Program to Replace the Character of a String
+public class StringReplace 
+{
+	public static void main(String[] args) 
+	{
+		String name="Yogendra Pratap Singh";
+		System.out.println(name.replace('Y', 'y'));        //yogendra Pratap Singh
+		System.out.println(name.replace('P', 'p'));        //Yogendra pratap Singh
+		System.out.println(name.replace('S', 's'));        //Yogendra Pratap singh
+	}
+
+}
