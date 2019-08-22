aa443271531254e4a4a247bb832b15db8a1086d0
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-23 00:48:37 +0530

    Add files via upload

---
+++ Last_Occurrence.java
@@ -0,0 +1,12 @@
+//Java Program to search the last position of a substring 
+public class Last_Occurrence 
+{
+	public static void main(String[] args) 
+	{
+		String name="Yogendra Pratap Singh";
+		int index=name.lastIndexOf("Pratap");          //Using variable.lastIndexof("Sub-String")
+		System.out.println(index);
+
+	}
+
+}
