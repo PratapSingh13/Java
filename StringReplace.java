d0ef77463e54e5565e75f6d4b6d72124af0a0913
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-23 01:28:08 +0530

    Add files via upload

--- StringReplace.java
+++ StringReplace.java
@@ -4,9 +4,10 @@ public class StringReplace
 	public static void main(String[] args) 
 	{
 		String name="Yogendra Pratap Singh";
-		System.out.println(name.replace('Y', 'y'));        //yogendra Pratap Singh
-		System.out.println(name.replace('P', 'p'));        //Yogendra pratap Singh
-		System.out.println(name.replace('S', 's'));        //Yogendra Pratap singh
+		System.out.println(name.replace('Y', 'y'));                          //yogendra Pratap Singh
+		System.out.println(name.replace('P', 'p'));                         //Yogendra pratap Singh
+		System.out.println(name.replace('S', 's'));                        //Yogendra Pratap singh
+		System.out.println(name.replaceAll("Pratap", "PRATAP"));          //Yogendra PRATAP Singh
 	}
 
 }
