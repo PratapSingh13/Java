b2243d3469caef543ec4f32de387ff154d2328bb
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-24 21:25:08 +0530

    Update RemoveDuplicacy_1.java

--- RemoveDuplicacy_1.java
+++ RemoveDuplicacy_1.java
@@ -4,9 +4,9 @@ public class RemoveDuplicacy_1
 	public static void main(String[] args) 
 	{
 		int a[]={1,2,2,4,5,5,6,7,7,8,9};
-		int temp[]=new int[a.length];                //Creating temporary Array of name temp[]
+		int temp[]=new int[a.length];                  //Creating temporary Array of name temp[]
 		int j=0;
-		for(int i=0; i<a.length-1; i++)              //i<a.length-1; because array should not go out of bound
+		for(int i=0; i<a.length-1; i++)                //i<a.length-1; because array should not go out of bound
 		{
 			if(a[i]!=a[i+1])
 			{
@@ -14,7 +14,7 @@ public class RemoveDuplicacy_1
 				j++;
 			}
 		}
-		temp[j]=a[a.length-1];                      //fix last element of a[] in temp[]
+		temp[j]=a[a.length-1];                          //fix last element of a[] in temp[]
 		for(int i=0; i<a.length; i++)
 		{
 			System.out.println(temp[i]+"");         //[1,2,,4,5,6,7,8,9,0,0,0,]
