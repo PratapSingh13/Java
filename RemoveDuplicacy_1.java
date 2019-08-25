20a68e973b647f85ed51a63f5ba9471183318617
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-25 16:32:02 +0530

    Add files via upload

--- RemoveDuplicacy_1.java
+++ RemoveDuplicacy_1.java
@@ -1,23 +1,24 @@
-//Java Program to Remove Duplicate Elements from an Array 
+//Java Program to Remove Duplicate Elements from an Sorted Array 
 public class RemoveDuplicacy_1 
 {
 	public static void main(String[] args) 
 	{
 		int a[]={1,2,2,4,5,5,6,7,7,8,9};
-		int temp[]=new int[a.length];                  //Creating temporary Array of name temp[]
+		int temp[]=new int[a.length];                //Creating temporary Array of name temp[]
 		int j=0;
-		for(int i=0; i<a.length-1; i++)                //i<a.length-1; because array should not go out of bound
+		for(int i=0; i<a.length-1; i++)              //i<a.length-1; because array should not go out of bound
 		{
 			if(a[i]!=a[i+1])
 			{
-				temp[j]=a[i];
-				j++;
+				temp[j]=a[i];                         // assigning the value of temp[j] 
+				j++;                                 //Increment j index;
 			}
 		}
-		temp[j]=a[a.length-1];                          //fix last element of a[] in temp[]
+		temp[j]=a[a.length-1];                      //fix last element of a[] in temp[]
 		for(int i=0; i<a.length; i++)
 		{
 			System.out.println(temp[i]+"");         //[1,2,,4,5,6,7,8,9,0,0,0,]
 		}
 	}
+
 }
