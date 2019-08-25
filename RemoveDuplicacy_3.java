238e54897c1e10b6c73fffe4795f527923f3db82
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-08-25 16:35:09 +0530

    Add files via upload

---
+++ RemoveDuplicacy_3.java
@@ -0,0 +1,21 @@
+//Java Program to Remove Duplicate Elements from Sorted and UnSorted Array 
+//By using HASH SET
+import java.util.HashSet;
+public class RemoveDuplicacy_3 
+{
+	public static void main(String[] args) 
+	{
+		int a[]= {1,3,2,4,2,3,4,6,5,5};
+		HashSet<Integer> HS=new HashSet<>();        //Creating an object of HashSet class
+		for(int i=0; i<a.length; i++)
+		{
+			HS.add(a[i]);
+		}
+		for(int no:HS)
+		{
+			System.out.println(no+"");
+		}
+
+	}
+
+}
