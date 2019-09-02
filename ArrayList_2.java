00848f49ce54b1d44f74b7877bee41e1316bb777
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-09-02 22:42:28 +0530

    Add files via upload

---
+++ ArrayList_2.java
@@ -0,0 +1,27 @@
+//ArrayList class is a re-sizable array, which is found in the java.util package.
+/*The difference between a built-in array and an ArrayList in Java, is that the size of an array cannot be modified 
+ *(if you want to add or remove elements to/from an array, we have to create a new one). 
+ * While elements can be added and removed from an ArrayList whenever you want. 
+ * The syntax is also slightly different:
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayList_2 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList<String> car=new ArrayList<String>();
+		car.add("VOLVO");
+		car.add("BMW");
+		car.add("AUDI");
+		car.add("FORD");
+		for(int i=0; i<car.size(); i++)
+		{
+			System.out.println(car.get(i));
+		}
+
+	}
+
+}
