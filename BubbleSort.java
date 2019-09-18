c856ee7e67d2f8b75162d5fdd63941d1b50d6951
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-09-18 23:09:01 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+// JAVA Program to Demonstrate Bubble Sort
+public class BubbleSort 
+{
+	public static void main(String[] args) 
+	{
+		int temp;
+		int [] array = {36,19,2,32,5,29};
+		
+		for(int i=0; i<array.length; i++)
+		{
+			int flag=0;
+			for(int j=0; j<(array.length-1)-i; j++)
+			{
+				if(array[j]>array[j+1])
+				{
+					temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					flag=1;
+				}
+			}
+			if(flag==0)
+			{
+				break;
+			}
+		}
+		for(int i=0; i<array.length; i++)
+		{
+			System.out.print(array[i]+ " ");
+		}
+
+	}
+
+}
