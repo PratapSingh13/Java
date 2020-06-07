c844c2c6925c4dc4e31fb89f7d000ab4ccfa0a91
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-06-07 23:25:39 +0530

    Create FloydTriangle.java

---
+++ FloydTriangle.java
@@ -0,0 +1,28 @@
+// Java Program to implement Floyd's triangle
+import java.util.Scanner;
+public class FloydTriangle 
+{
+	public static void main(String[] args) 
+	{
+		int rows;
+		int counter_i, counter_j;
+		int number = 1;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number of rows for Floyd's Triangle:");
+		
+		rows = scan.nextInt();
+		System.out.println("Floyd's Triangle");
+		
+		for(counter_i = 1; counter_i <= rows; counter_i++)
+		{
+			for(counter_j = 1; counter_j <= counter_i; counter_j++)
+			{
+				System.out.print(number+" ");
+				number++;              // For Incrementing the number value
+			}
+			System.out.println();      // To change the line
+		}
+
+	}
+
+}
