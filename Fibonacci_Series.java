a43d3be837a52f985242b5df484a8d45f2c309af
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-11-08 11:42:24 +0530

    Update Fibonacci_Series.java

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -5,19 +5,19 @@ public class Fibonacci_Series
 	public static void main(String[] args) 
 	{
 		int num;
-		int a=0;
-		int b=0;
-		int c=1;
+		int num1=0;
+		int num2=0;
+		int num3=1;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number of Times");
 		num=sc.nextInt();
 		System.out.println("Fibonacci Series of the Number is:");
 		for(int i=0; i<=num; i++)
 		{
-		    a=b;
-		    b=c;
-		    c=a+b;
-		    System.out.println(a +"");
+		    num1=num2;
+		    num2=num3;
+		    num3=num1+num2;
+		    System.out.println(num1 +"");
 		}   
 
 	}
